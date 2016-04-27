dff8f5c69abf82808d5c8af4c634588e710335c9
Andreas Pritschet <andreas@pritschet.eu>
2016-04-27 14:28:26 +0200

    added ui example

---
+++ Assignment2/ui.py
@@ -0,0 +1,55 @@
+import sys
+from PyQt4 import QtGui
+
+
+class Example(QtGui.QWidget):
+
+    draw = False
+
+    def __init__(self):
+        super(Example, self).__init__()
+
+        self.initUI()
+        self.test = self
+
+    def initUI(self):
+
+        self.setGeometry(400, 400, 400, 400)
+
+        qbtn = QtGui.QPushButton('Paint', self)
+        qbtn.clicked.connect(self.buttn)
+        qbtn.resize(qbtn.sizeHint())
+        qbtn.move(50, 50)
+
+        self.setGeometry(300, 300, 250, 150)
+        self.setWindowTitle('Paint')
+        self.show()
+
+    def paintEvent(self, e):
+
+        qp = QtGui.QPainter()
+        qp.begin(self)
+        self.drawRectangles(qp)
+        qp.end()
+
+    def drawRectangles(self, qp):
+
+        if self.draw:
+            qp.setBrush(QtGui.QColor(000, 000, 255))
+            qp.drawRect(0, 0, 90, 60)
+
+    def buttn(self):
+        self.draw = True
+        self.update()
+
+
+def main():
+
+    app = QtGui.QApplication(sys.argv)
+    Example()
+
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
