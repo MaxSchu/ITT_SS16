2259d9a10509345c25976403fd673322aa7fbb64
Andreas Pritschet <andreas@pritschet.eu>
2016-04-29 12:11:49 +0200

    added code

--- Assignment2/ui.py
+++ Assignment2/ui.py
@@ -1,6 +1,7 @@
 import sys
 from PyQt5 import QtGui, QtWidgets, QtCore
 from threading import Timer
+import time
 import random
 
 
@@ -14,18 +15,25 @@ class Example(QtWidgets.QWidget):
         self.countdown = 0
         self.maxRepetitions = 10
         self.repetitions = 0
-        self.sign = ("X", "O")
         self.keys = (QtCore.Qt.Key_V, QtCore.Qt.Key_B)
         self.keyMapping = {self.keys[0]: "V", self.keys[1]: "B"}
-        self.mode = 1
+        self.mode = 0
+        self.backgroundMode = 0
         self.uiBlocked = False
         self.showRect = True
         self.counter = 0
+        self.timestampBuffer = 0
+        self.correctKey = ""
         self.initUI()
+        self.instructions = "Anleitung"
 
     def initUI(self):
         self.setGeometry(100, 100, 1000, 800)
         self.setWindowTitle('Paint')
+        palette = QtGui.QPalette()
+        palette.setBrush(
+            QtGui.QPalette.Background, QtGui.QBrush(QtGui.QPixmap("test.jpg")))
+        self.setPalette(palette)
         self.show()
         self.showIntroMessage()
 
@@ -68,28 +76,52 @@ class Example(QtWidgets.QWidget):
         if self.uiBlocked:
             return
 
-        if (ev.key() == self.keys[0] or ev.key() == self.keys[1]):
-            print(self.keyMapping[ev.key()])
-            self.uiBlocked = True
-            self.repetitions -= 1
+        try:
+            pressedKey = chr(ev.key())
+        except ValueError:
+            pressedKey = "KeyCode: " + str(ev.key())
 
-            if self.repetitions > 0:
-                t = Timer(1.0, self.newSign)
-                t.start()
-            else:
-                self.showIntroMessage()
-            self.showRect = False
-            self.update()
+        print("Key pressed: " + pressedKey)
+
+        # TODO: save current Timestamp
+        # TODO: save reactionTime
+        reactionTime = time.time() - self.timestampBuffer
+        print ("reactionTime: " + str(reactionTime))
+
+        if self.correctKey == ev.key():
+            # TODO: set correctKeyPressed to true
+            print("Correct!")
+        else:
+            # TODO: set correctKeyPressed to false
+            print("Wrong!")
+
+        self.uiBlocked = True
+        self.repetitions -= 1
+
+        if self.repetitions > 0:
+            t = Timer(1.0, self.newSign)
+            t.start()
+        else:
+            self.showIntroMessage()
+        self.showRect = False
+        self.update()
 
     def drawRectangle(self, qp):
         if self.showRect:
             qp.setBrush(QtGui.QColor(255, 255, 255))
             qp.drawRect(self.position[0], self.position[1], 100, 100)
             rect = QtCore.QRect(self.position[0], self.position[1], 100, 100)
+            self.timestampBuffer = time.time()
 
             if self.mode == 0:
+                randomInt = random.randint(1, 8)
                 qp.drawText(
-                    rect, QtCore.Qt.AlignCenter, str(random.randint(1, 8)))
+                    rect, QtCore.Qt.AlignCenter, str(randomInt))
+                self.correctKey = list((self.keyMapping).keys())[randomInt % 2]
+
+                # TODO: set requiredKeyPressed to
+                # self.keyMapping[self.correctKey]
+                print(self.keyMapping[self.correctKey])
             else:
                 qp.setBrush(QtGui.QColor(0, 0, 255))
                 if random.randint(0, 1) == 0:
@@ -103,9 +135,9 @@ class Example(QtWidgets.QWidget):
 
     def drawIntro(self, qp):
         qp.drawText(
-            QtCore.QRect(100, 100, 900, 300), QtCore.Qt.AlignCenter, "Hier steht die Anleitung")
+            QtCore.QRect(100, 100, 800, 300), QtCore.Qt.AlignCenter, self.instructions)
         qp.drawText(
-            QtCore.QRect(100, 200, 900, 300), QtCore.Qt.AlignCenter, str(self.countdown))
+            QtCore.QRect(100, 200, 800, 300), QtCore.Qt.AlignCenter, str(self.countdown))
 
 
 def main():
