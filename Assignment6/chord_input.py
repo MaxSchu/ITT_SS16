d8df823b27b6218a83bce55b3704c84de9512b90
Andreas Pritschet <andreas@pritschet.eu>
2016-05-29 21:05:33 +0200

    added comments and error handling - refactoring

--- Assignment6/chord_input.py
+++ Assignment6/chord_input.py
@@ -7,17 +7,14 @@ import re
 
 
 class TextLogger(QtWidgets.QTextEdit):
-
-    def __init__(self, example_text=""):
+    def __init__(self):
         super(TextLogger, self).__init__()
-        self.setText(example_text)
         self.initUI()
 
     def initUI(self):
         self.setGeometry(0, 0, 400, 400)
         self.setWindowTitle('TextLogger')
         self.setFocusPolicy(QtCore.Qt.StrongFocus)
-        # self.setMouseTracking(True)
         self.show()
 
     def keyPressEvent(self, ev):
@@ -41,7 +38,9 @@ class ChordInputMethod(QtCore.QObject):
         self.chords = self.readChords()
 
     def readChords(self):
+        # read the possible chords from a file
         chords = {}
+        frozenchord = {}
         chord = ""
         words = ""
 
@@ -49,29 +48,57 @@ class ChordInputMethod(QtCore.QObject):
         temp = file.read().splitlines()
 
         for line in temp:
-            chord = (line.split(":"))[0]
-            words = ((line.split(":"))[1]).split(",")
-            chords.update({frozenset(list(chord)): words})
+            try:
+                # read chord combination
+                chord = (line.split(":"))[0]
+                # create frozenset to use as key in dictionary
+                frozenchord = frozenset(list(chord))
+
+                if len(chord) == 1:
+                    print("Hint - Chord should be at least two characters long: " + chord)
+
+                # read possible translations for chord
+                words = ((line.split(":"))[1]).split(",")
+
+                if frozenchord in chords:
+                    print("Warning - Duplicate Chord: " + chord)
+                    # add possible translations if chord is a duplicate
+                    words.extend(chords[frozenchord])
+
+                chords.update({frozenchord: words})
+            except IndexError:
+                print("Could not read chord " + line + "!")
 
         file.close()
         return chords
 
+    def resetChordCounter(self):
+        self.keys = []
+        self.chordCode = []
+        self.chordSwitcher = False
+        self.counter = 0
+
     def getWord(self):
-        uppercase = False
+        titleCase = False
         compareSet = []
 
         try:
             for s in self.keys:
                 if s.isupper():
-                    uppercase = True
+                    # check if any character in chord is uppercase
+                    # -> make word titlecase if true
+                    titleCase = True
+                # set has to be lowercase when compared to possible chords
                 compareSet.append(s.lower())
 
             if " " in compareSet:
+                # remove 'space' when comparing set to chards
                 compareSet.remove(" ")
             if len(self.chords[frozenset(compareSet)]) <= self.counter:
+                # avoud out of bounds exception
                 self.counter = 0
 
-            if (uppercase):
+            if (titleCase):
                 return ((self.chords[frozenset(compareSet)]))[self.counter].title()
             else:
                 return ((self.chords[frozenset(compareSet)]))[self.counter]
@@ -81,31 +108,34 @@ class ChordInputMethod(QtCore.QObject):
             return "".join(self.keys)
 
     def removePastLetter(self, watched_textedit, count):
+        # remove characters from widget
         for x in range(0, count):
             Qt.qApp.postEvent(watched_textedit, QtGui.QKeyEvent(
                 Qt.QKeyEvent.KeyPress, QtCore.Qt.Key_Backspace, QtCore.Qt.NoModifier))
 
     def eventFilter(self, watched_textedit, ev):
         if not ev.spontaneous():
-            return False  # ignore events that we injected ourselves
+            # ignore events that we injected ourselves
+            return False
         if not ev.type() in [Qt.QKeyEvent.KeyPress, Qt.QKeyEvent.KeyRelease]:
-            return False  # ignore everything else
+            # ignore everything else
+            return False
         if re.match(ChordInputMethod.VALID_LETTERS, ev.text().lower()) is None:
-            return False  # only check this _after_ we are sure that we have a QKeyEvent!
-        # TODO: allow autorepeat when only a single key is pressed at the
-        # moment
-        if ev.isAutoRepeat():  # completely eliminate these, as they might interfere with chords!
+            # only check this _after_ we are sure that we have a QKeyEvent!
+            return False
+        if ev.isAutoRepeat():
+            # completely eliminate these, as they might interfere with chords!
             return True
         # finally, we only have interesting key presses/releases left
         if ev.type() == Qt.QKeyEvent.KeyPress:  # collect keys
             self.keys.append(ev.text())
 
             if self.chordSwitcher:
-                print(self.chordCode)
                 if ev.text() == " " and self.keys == self.chordCode:
-                    print(len(self.getWord()))
+                    # replace the old word...
                     self.removePastLetter(
                         watched_textedit, len(self.getWord()))
+                    # ...and switch through alternatives
                     self.counter += 1
                     Qt.qApp.postEvent(watched_textedit, QtGui.QKeyEvent(
                         Qt.QKeyEvent.KeyPress, 0, QtCore.Qt.NoModifier, text=self.getWord()))
@@ -114,15 +144,17 @@ class ChordInputMethod(QtCore.QObject):
                 self.chordCode = []
                 self.chordSwitcher = False
 
-            return False  # always filter press events
+            # always filter press events
+            return False
         elif ev.type() == Qt.QKeyEvent.KeyRelease:  # release chord once one of the keys is released
             if " " not in self.keys:
-                self.keys = []
-                self.chordCode = []
-                self.chordSwitcher = False
-                self.counter = 0
+                # space has to be used to build a chord
+                self.resetChordCounter()
                 return False
-            if len(self.keys) > 2 and not self.chordSwitcher:
+
+            # chords have to be at least one character + space
+            if len(self.keys) > 1 and not self.chordSwitcher:
+                # if chord is used, remove the past characters first
                 self.removePastLetter(
                     watched_textedit, len(self.keys))
                 result = self.getWord()
@@ -130,18 +162,17 @@ class ChordInputMethod(QtCore.QObject):
                     Qt.QKeyEvent.KeyPress, 0, QtCore.Qt.NoModifier, text=result))
 
                 if (ev.text() != " "):
-                    self.counter = 0
-                    self.chordCode = []
-                    self.chordSwitcher = False
-                    self.keys = []
+                    self.resetChordCounter()
                 else:
+                    # if only space is released, the user wants to switch to aternative maybe
                     self.chordCode = self.keys
                     self.keys.remove(" ")
                     self.chordSwitcher = True
                     return False
             if ev.text() in self.keys:
+                # remove released buttons from set
                 self.keys.remove(ev.text())
-            return True  # also when non-printables are released (sensible?)
+            return True
         else:
             print("Should'nt arrive here: " + str(ev))
             return False
@@ -149,7 +180,7 @@ class ChordInputMethod(QtCore.QObject):
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
-    text_logger = TextLogger("Es war einmal ein Mann.")
+    text_logger = TextLogger()
     chord_input = ChordInputMethod()
     text_logger.installEventFilter(chord_input)
 
