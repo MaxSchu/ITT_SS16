4732ef732575bcc7f43296ed79978856be258ece
Andreas Pritschet <andreas@pritschet.eu>
2016-06-08 17:28:33 +0200

    done stuff with calculat0r

--- Assignment9/calculator/calculator.py
+++ Assignment9/calculator/calculator.py
@@ -1,28 +1,39 @@
-from PyQt5 import uic, QtWidgets, QtCore
-from functools import partial
+from PyQt5 import uic, QtWidgets
+from enum import Enum
 import sys
 
 
 class Calculator(QtWidgets.QMainWindow):
 
-    BUTTONS = {}
+    class Operations(Enum):
+        ADD, SUB, MUL, DIV, REMOVE, CLEAR, EQUALS = range(7)
 
     def __init__(self):
         super(self.__class__, self).__init__()
         self.ui = uic.loadUi('calculator.ui', self)
+        self.currentValue = 0
         self.setupButtons()
 
     def addNumber(self, text):
-        self.ui.textBrowser.append(text)
+        self.ui.textBrowser.insertPlainText(text)
+        print(float(self.ui.textBrowser.toPlainText()))
+
+    def performOperation(self, op):
+        print(op)
 
     def setupButtons(self):
-        BUTTONS = {0: self.ui.number_0, 1: self.ui.number_1, 2: self.ui.number_2, 3: self.ui.number_3,
+        NUMBERS = {0: self.ui.number_0, 1: self.ui.number_1, 2: self.ui.number_2, 3: self.ui.number_3,
                    4: self.ui.number_4, 5: self.ui.number_5, 6: self.ui.number_6, 7: self.ui.number_7,
-                   8: self.ui.number_8, 9: self.ui.number_9}
+                   8: self.ui.number_8, 9: self.ui.number_9, ".0": self.ui.funcDec}
 
-        for button in BUTTONS.keys():
-            print (button)
-            BUTTONS[button].clicked.connect(partial(self.addNumber, str(button)))
+        OPERATORS = {self.Operations.ADD: self.ui.opAdd, self.Operations.SUB: self.ui.opSub,
+                     self.Operations.MUL: self.ui.opMul, self.Operations.DIV: self.ui.opDiv}
+
+        for number in NUMBERS.keys():
+            NUMBERS[number].clicked.connect(lambda ignore, x=number: self.addNumber(str(x)))
+
+        for operator in OPERATORS.keys():
+            OPERATORS[operator].clicked.connect(lambda ignore, x=operator: self.performOperation(x))
 
 
 def main():
