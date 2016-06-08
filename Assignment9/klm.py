e7d8e267b2fa77300e55dcd2b55d3d1dac3607fe
MaxSchu <maximilian.schubert@stud.uni-regensburg.de>
2016-06-08 17:48:47 -0400

    adsf

--- Assignment9/klm.py
+++ Assignment9/klm.py
@@ -35,8 +35,8 @@ def main():
     expDict = {"k": 0.14, "p": 0.5, "b": 0.11, "h": 0.54, "m": 1.2, "w": 0.0}
 
     klm = KLMCalculator()
-    print("Original KLM operators: " + str(klm.calcKLM(klmDict)))
-    print("Own measured KLM operators: " + str(klm.calcKLM(expDict)))
+    print("Original KLM operators (prediction time in seconds): " + str(klm.calcKLM(klmDict)))
+    print("Own measured KLM operators (prediction time in seconds): " + str(klm.calcKLM(expDict)))
 
 
 if __name__ == '__main__':
