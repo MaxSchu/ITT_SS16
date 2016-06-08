032080f013b6568ab823c0b4abb5d53b14d8703b
MaxSchu <maximilian.schubert@stud.uni-regensburg.de>
2016-06-08 15:05:22 -0400

    asdf

---
+++ Assignment9/klm.py
@@ -0,0 +1,40 @@
+import sys
+
+
+class KLMCalculator():
+    def calcKLM(self, dictionary):
+        time = 0.0
+        if (len(sys.argv) == 2):
+            file = open(sys.argv[1])
+            for line in file:
+                line = line.replace("\n", "")
+                line = line.partition('#')[0]
+                line = line.rstrip()
+                time += self.calcKLMForLine(line, dictionary)
+        return time
+
+    def calcKLMForLine(self, line, dictionary):
+        lineKLM = 0.0
+        number = ""
+        for c in line:
+            c = c.lower()
+            if c.isdigit():
+                number += c
+            else:
+                if number != "":
+                    lineKLM += int(number) * dictionary[c]
+                    number = ""
+                else:
+                    lineKLM += dictionary[c]
+        return lineKLM
+
+
+def main():
+    klmDict = {"k": 0.28, "p": 1.1, "b": 0.1, "h": 0.4, "m": 1.2, "w": 0.0}
+
+    klm = KLMCalculator()
+    print("Card, Moran, Newell and Kieras: " + str(klm.calcKLM(klmDict)))
+
+
+if __name__ == '__main__':
+    main()
